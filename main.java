e37757e616c2069732055cbb938cd6f9c946c003
Moritz Lingens <minecraft-mo@posteo.de>
2025-02-22 19:37:05 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+import src.*;
+
+public class main {
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        ui.launch(ui.class, args);
+        javafx.stage.Stage stage = new javafx.stage.Stage();
+    }
+}
