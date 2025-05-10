6abdd8c5470d28518fc50231a391940983c301bf
Lennart Kassal <113629067+schlaumischlumpf@users.noreply.github.com>
2025-05-10 21:23:57 +0200

    Delete Wordle/Wordle-main/Wordle-main directory

--- Wordle/Wordle-main/Wordle-main/module-info.java
+++
@@ -1,9 +0,0 @@
-module Wordle {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports src;
-    exports ressource;
-    opens src to javafx.fxml;
-    opens ressource to javafx.fxml;
-}
